a2a32d1e0e6d08803afe8399d57b2c4927818045
craig-humby <craig.humby@myport.ac.uk>
2013-10-03 15:46:19 +0100

    committ

---
+++ javascript/addcontacts.js
@@ -0,0 +1,22 @@
+window.onload=function()
+{
+		document.addEventListener("deviceready"), onDeviceReady, false);
+}
+
+function onDeviceReady()
+{
+	document.getElementById("btnSave").addEventListener("click", saveContact, false);
+}
+
+function saveContact()
+{
+	var firstName = document.getElementById("first").value;
+	var lastName = document.getElementById("last").value;
+	var telephoneNumber = document.getElementById("number").value;
+	var phoneType = document.getElementById("type").value;
+	
+	var fullName = firstName + " " + lastName;
+	var cntContact = navigator.contacts.create({"displayName":fullName})
+	
+	cntContact.save();
+}
